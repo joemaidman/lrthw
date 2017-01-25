c87f4e6840e701e00eefa68031b491d98140ed55
Joe Maidman <joemaidman@gmail.com>
2017-01-25 18:50:32 +0000

    Exercise 1 with drills

---
+++ ex1a.rb
@@ -0,0 +1,8 @@
+puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing stuff."
+#puts "This is fun."
+#puts "Yay! Printing."
+#puts "I'd rather you 'not'."
+#puts 'I "said" do not touch this.'
+puts "Printing yet another line!!"
