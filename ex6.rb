b10445617a18f5aa7f2138682e3c09e9ef519678
Joe Maidman <joemaidman@gmail.com>
2017-01-26 12:12:29 +0000

    Exercise 6

---
+++ ex6.rb
@@ -0,0 +1,30 @@
+# Declare a variabe called types_of_people with a value of 10
+types_of_people = 10
+# Declare a variable called x with a string value that includes interpolation from the variable types_of_people
+x = "There are #{types_of_people} types of people."
+# Declare a variable called binary with a string value of binary
+binary = "binary"
+# Declare a variable called do_not with a string value of don't
+do_not = "don't"
+# Declare a variable called y with a string value that includes interpolation from the variables binary and do_not
+y = "Those who know #{binary} and those who #{do_not}."
+# Print the value of the variable x to the screen
+puts x
+# Print the value of the variable y to the screen
+puts y
+# Print a string to the screen including interpolating the value of the variable x
+puts "I said: #{x}."
+# Print a string to the screen including interpolating the value of the variable y
+puts "I also said: '#{y}'."
+# Declare a variable called hilarious and give it a boolean value of false
+hilarious = false
+# Declare a variable called joke_evaluation with a string value that includes interpolation from the variable hilarious
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+# Pring the value of the variabe joke_evaluation to the screen
+puts joke_evaluation
+# Declare a vairable called w and give it a string value
+w = "This is the left side of..."
+# Declare a variable called e and give it a string value
+e = "a string with a right side."
+# Print the concatentated values of the variables w and e to the screen for another hilarious joke!
+puts w + e
