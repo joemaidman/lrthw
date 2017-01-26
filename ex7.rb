e11586acfbcd06aa2d05ffda4d51087041de8cc5
Joe Maidman <joemaidman@gmail.com>
2017-01-26 12:20:56 +0000

    Exercise 7

---
+++ ex7.rb
@@ -0,0 +1,28 @@
+# Print some text to the screen
+puts "Mary had a little lamb."
+# Print a string to the screen
+puts "Its fleece was white as #{'snow'}."
+# Print a string to the screen
+puts "And everywhere that Mary went."
+# Print a fullstop to the screen ten times
+puts "." * 10 # what's that do?
+# Declare 12 variables and give them all a character
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+# puts adds a new line after, print does not!
+# print and concatenate the first six character variables
+print end1 + end2 + end3 + end4 + end5 + end6
+# puts and concatenate the last six character variables
+puts end7 + end8 + end9 + end10 + end11 + end12
