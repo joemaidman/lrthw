84e11c5583dbf75e328cc3b79e424d7e12db95e0
Joe Maidman <joemaidman@gmail.com>
2017-01-29 13:21:52 +0000

    Exercise 18

---
+++ ex18a.rb
@@ -0,0 +1,8 @@
+# Functions checklist:
+# 1) Start with 'def'
+# 2) name should start with a letter and can contain letters, numbers of underscores
+# 3) puts arguments inside ()
+# 4) arguments need unique names
+# 5) indent the lines inside the function by two spaces
+# 6) end the function with 'end'
+# 7) Functions that don't take any arguments don't need to be called with (), but it may make sense to include them fro readability.
