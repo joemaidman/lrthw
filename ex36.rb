c687b9dd76f79f94adc3e3f705110ca77d1ec7be
Joe Maidman <joemaidman@gmail.com>
2017-01-29 23:31:04 +0000

    Exercise 36

---
+++ ex36.rb
@@ -0,0 +1,23 @@
+# RULES FOR IF STATEMENTS
+=begin
+1) Every if must have an else
+2) If the else should never actually be triggered, you must
+use a die function inside the else that prints an error message
+e.g. exit(1)
+3) Never nest ifs more than two deep and preferably one at most
+4) Put blank lines before and afer
+5) Keep the boolean tests simple.
+=end
+
+# RULES FOR LOOPS
+=begin
+1) Only use a while to loop forever i.e. never!
+2) Then use a for loop for everything else.
+=end
+
+# RULES FOR DEBUGGING
+=begin
+1) Don't use a debugger
+2) Best to use puts to get values of variables to see if they are wrong
+3) Test as you go.
+=end
